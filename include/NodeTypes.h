b7bb1ca7624c00865102766c6dc931f962dbaf10
Croc Dialer <crocdialer@gmail.com>
2020-05-10 13:04:48 +0200

    include addresses in checksum

--- include/NodeTypes.h
+++ include/NodeTypes.h
@@ -3,23 +3,16 @@
 
 #include <stdint.h>
 
-#define STRUCT_TYPE_WEATHERMAN 0x77
+#define STRUCT_TYPE_TEMPERATUREMAN 0x76
 
-typedef struct weather_t
+typedef struct temperature_t
 {
-    uint8_t stype = STRUCT_TYPE_WEATHERMAN;
+    uint8_t stype = STRUCT_TYPE_TEMPERATUREMAN;
 
     uint8_t battery = 0;
 
-    //! temperature in range [-100C .. 100C]
+    //! temperature in range [-50C .. 100C]
     uint16_t temperature = 0;
-
-    //! pressure in range [0hPa .. 2000hPa]
-    uint16_t pressure = 0;
-
-    // relative humidity in range [0..1]
-    uint8_t humidity = 0;
-
-} weather_t;
+} temperature_t;
 
 #endif
--- include/device_id.h
+++ include/device_id.h
@@ -1,4 +1,4 @@
 #pragma once
 
-#define DEVICE_ID "WEATHERMAN"
+#define DEVICE_ID "TEMPERATUREMAN"
 #define CMD_QUERY_ID "ID"
